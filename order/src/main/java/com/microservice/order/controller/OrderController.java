c8b3a3618b578e5a38ac783fd0b91d050e07055d
Yuthika <yuthikavj@gmail.com>
2026-04-07 13:03:20 +0530

    kafka done

--- order/src/main/java/com/microservice/order/controller/OrderController.java
+++ order/src/main/java/com/microservice/order/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.microservice.order.controller;
 
+import com.base.base.dto.OrderEventDTO;
 import com.microservice.order.common.OrderResponse;
 import com.microservice.order.dto.OrderDTO;
+import com.microservice.order.kafka.OrderProducer;
 import com.microservice.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,13 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private final OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
     @GetMapping("/getorders")
     public List<OrderDTO> getOrders() {
         return orderService.getAllOrders();
@@ -28,6 +37,10 @@ public class OrderController {
 
     @PostMapping("addorder")
     public OrderResponse saveOrder(@RequestBody OrderDTO orderDTO) {
+        OrderEventDTO orderEventDTO = new OrderEventDTO();
+        orderEventDTO.setMessage("Order committed");
+        orderEventDTO.setStatus("pending");
+        orderProducer.sendMessage(orderEventDTO);
         return orderService.saveOrder(orderDTO);
     }
 
